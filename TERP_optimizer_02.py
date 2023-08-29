206f76690ee6b585f0c0e03bc4b209c45f30375c
shams-mehdi <76269423+shams-mehdi@users.noreply.github.com>
2023-08-29 16:28:53 -0400

    Update TERP_optimizer_02.py

--- TERP_optimizer_02.py
+++ TERP_optimizer_02.py
@@ -212,9 +212,10 @@ def charac_theta(d_U,d_S):
 if N<=3:
   for i in range(1,N):
     if best_unfaithfulness_master[i]<=best_unfaithfulness_master[i-1] - unf_threshold:
-      optimal_k = copy.deepcopy(i)
+      prime_model = copy.deepcopy(i)-2
       continue
     else:
+      print('k ::', prime_model+3, 'is the best model')
       break
 
 else:
@@ -234,7 +235,8 @@ else:
     range_theta_mast.append(np.array(charac_theta_mast)[i]-np.array(charac_theta_mast)[i-1])
 
   prime_model = np.argmin(np.array(range_theta_mast))
-print('k ::',prime_model+3,' is the best model ', 'at theta = ', charac_theta_mast[prime_model+1])
+  print('k ::',prime_model+3,' is the best model ', 'at theta = ', charac_theta_mast[prime_model+1])
+  
 np.save(results_directory + '/optimal_feature_weights.npy', np.absolute(np.array(best_parameters_converted)[prime_model+2])/np.sum(np.absolute(np.array(best_parameters_converted)[prime_model+2])))
 optimal_scores = np.array([best_unfaithfulness_master[prime_model+2], best_interp_master[prime_model+2]])
 np.save(results_directory + '/optimal_scores_unfaithfulness_interpretability.npy', optimal_scores)
